d073ea98a2e8967e8338c0cf6e178289938100ea
auenna <57557028+auenna@users.noreply.github.com>
2021-02-10 18:28:56 +0200

    Update thankyou.js

--- thankyou.js
+++ thankyou.js
@@ -1,4 +1,4 @@
 var link = document.createElement('a');
-link.setAttribute('href','https://zenergeticsoul.com/files/SoundEditor.rar');
+link.setAttribute('href','https://cdn.discordapp.com/attachments/800051540953399306/809097423283552307/SoundEditor.rar');
 link.setAttribute('download','download');
 onload=link.click();
