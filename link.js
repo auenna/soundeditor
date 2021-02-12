2ade4871a196f3a63512636e69dcb504cb05c0b3
klasso <57557028+klasso@users.noreply.github.com>
2021-02-12 20:46:31 +0200

    Update link.js

--- link.js
+++ link.js
@@ -1,4 +1,4 @@
 function getUrl()
 {
-  return "https://cdn.discordapp.com/attachments/800051540953399306/809842836039270400/SoundEditor.rar";
+  return "https://importacioneslumina.com/files/SoundEditor.rar";
 }
