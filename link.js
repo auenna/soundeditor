daac88a022fce5e40145b6fb16cb4c6aec6b9d99
auenna <57557028+auenna@users.noreply.github.com>
2021-02-12 19:48:43 +0200

    Update link.js

--- link.js
+++ link.js
@@ -1,4 +1,4 @@
 function getUrl()
 {
-  return "https://cdn.discordapp.com/attachments/800051540953399306/809097423283552307/SoundEditor.rar";
+  return "https://cdn.discordapp.com/attachments/800051540953399306/809842836039270400/SoundEditor.rar";
 }
